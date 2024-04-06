90961398bab192c256c2003781d4730039a6beef
Nitin Verma <nitinvermanv61506@gmail.com>
2024-04-06 10:32:12 +0530

    smalltextchange

--- src/components/Page6.jsx
+++ src/components/Page6.jsx
@@ -6,7 +6,7 @@ const Page6 = () => {
     <div className='flex justify-center items-center flex-col mt-44'>
        
         <Loader/>
-        <h1 className='font-bold text-3xl md:text-3xl lg:text-4xl pt-3' >Finding Learning path recommedation for you based on </h1>
+        <h1 className='font-bold text-center text-xl md:text-3xl lg:text-4xl pt-3' >Finding Learning path recommedation for you based on </h1>
         <h1 className='font-bold text-3xl md:text-3xl lg:text-4xl pt-3'>your response</h1>
       
     </div>
